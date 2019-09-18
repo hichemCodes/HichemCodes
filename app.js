9aa5ad9eb58b4f2c4c3289562a61b91cd9c1d060
hichemCodes <laouarmedhichem23@gmail.com>
2019-09-19 00:45:44 +0100

    update file

--- app.js
+++
@@ -1,9 +0,0 @@
-
-const burger = document.querySelector('.burger');
-const link =  document.querySelector('.link');
-
-burger.addEventListener('click',()=>{
-     link.classList.toggle('open');
-    burger.classList.toggle('toggle')
-})
-
